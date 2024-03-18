e50f21d7f0d3c1f214363af573aadf6ee7360316
MikeyZam100 <157930808+MikeyZam100@users.noreply.github.com>
2024-03-17 18:16:47 -0700

    Added first 9 pokemon to the array 'pokemonList'

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-alert('Hello World!');
+let pokemonList = [
+    {name:Bulbasaur, type:grass}, 
+    {name:Ivysaur, type:grass}, 
+    {name:Venusaur, type:grass, poison},
+    {name:Charmander, type:fire},
+    {name:Charmeleon, type:fire},
+    {name:Charizard, type:fire},
+    {name:Squirtle, type:water},
+    {name:Wartortle, type:water},
+    {name:Blastoise, type:water},
+];
