755168638dc62fd8ae3e3fa76443aa533418973d
MikeyZam100 <157930808+MikeyZam100@users.noreply.github.com>
2024-03-20 21:24:04 -0700

    created a for loop displaying a list of all pokemon in the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 let pokemonList = [
-    {name:Bulbasaur, height:'0.7 m', type:grass}, 
-    {name:Ivysaur, height:'1 m', type:grass}, 
-    {name:Venusaur, height:'2 m', type:grass, poison},
-    {name:Charmander, height:'0.6 m,', type:fire},
-    {name:Charmeleon, height:'1.1 m', type:fire},
-    {name:Charizard, height:'1.7 m', type:fire},
-    {name:Squirtle, height:'0.5 m', type:water},
-    {name:Wartortle, height:'1 m', type:water},
-    {name:Blastoise, height:'1.6 m', type:water},
+    {name:'Bulbasaur', height:'0.7 m', type:'grass'}, 
+    {name:'Ivysaur', height:'1 m', type:'grass'}, 
+    {name:'Venusaur', height:'2 m', type:'grass,' + ' poison'},
+    {name:'Charmander', height:'0.6 m,', type:'fire'},
+    {name:'Charmeleon', height:'1.1 m', type:'fire'},
+    {name:'Charizard', height:'1.7 m', type:'fire'},
+    {name:'Squirtle', height:'0.5 m', type:'water'},
+    {name:'Wartortle', height:'1 m', type:'water'},
+    {name:'Blastoise', height:'1.6 m', type:'water'},
 ];
+
+// displays the items onto the page
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ': ' + pokemonList[i].type)
+}
