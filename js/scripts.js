e531dd41ea4fe22ab39c35b193f0096570da1faf
MikeyZam100 <157930808+MikeyZam100@users.noreply.github.com>
2024-03-30 23:25:26 -0700

    tested out new code ideas
    changed the forEach loop to a function named addListItem
    
    within said function, added an event listener connected to the showDetails function, which logs the clicked button into the console
    
    then called the functions outside the IIFE to render onto the page
    
    Finally I copy/pasted all these changes onto the proper scripts.js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,58 @@
 let pokemonRepository = (function() {
-    let pokemonList = [
-      { name:'Bulbasaur', height:'0.7 m', type:'grass' },
-      { name:'Ivysaur', height:'1 m', type:'grass' },
-      { name:'Venusaur', height:'2 m', type:'grass, poison' },
-      { name:'Charmander', height:'0.6 m', type:'fire' },
-      { name:'Charmeleon', height:'1.1 m', type:'fire' },
-      { name:'Charizard', height:'1.7 m', type:'fire' },
-      { name:'Squirtle', height:'0.5 m', type:'water' },
-      { name:'Wartortle', height:'1 m', type:'water' },
-      { name:'Blastoise', height:'1.6 m', type:'water' }
-    ];
-  
-    function getAll() {
-      return pokemonList;
-    }
-  
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
-    pokemonList.forEach(function (pokemon) {
-      let pokemonList = document.querySelector('.pokemon-list');
-      let listItem = document.createElement('li');
-      let button = document.createElement('button');
+  let pokemonList = [
+    { name:'Bulbasaur', height:'0.7 m', type:'grass' },
+    { name:'Ivysaur', height:'1 m', type:'grass' },
+    { name:'Venusaur', height:'2 m', type:'grass, poison' },
+    { name:'Charmander', height:'0.6 m', type:'fire' },
+    { name:'Charmeleon', height:'1.1 m', type:'fire' },
+    { name:'Charizard', height:'1.7 m', type:'fire' },
+    { name:'Squirtle', height:'0.5 m', type:'water' },
+    { name:'Wartortle', height:'1 m', type:'water' },
+    { name:'Blastoise', height:'1.6 m', type:'water' }
+  ];
 
-      button.innerText = 'pokemon.name';
-      button.classList.add('pokemon-button');
+  function getAll() {
+    return pokemonList;
+  }
 
-      listItem.appendChild('button');
-      pokemonList.appendChild('listItem');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  function addListItem(pokemon) {
+    let pokemonListElement = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
     });
+
+    listItem.appendChild(button);
+    pokemonListElement.appendChild(listItem);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails
+  };
+})();
+
+document.addEventListener('DOMContentLoaded', function() {
+  let pokemonList = pokemonRepository.getAll();
+  let pokemonListElement = document.querySelector('.pokemon-list');
+
+  pokemonList.forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+
+  });
 });
-  
-  pokemonRepository();
